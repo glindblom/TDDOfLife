60ecab8f6c9d741892220bf2c04486891af74bb8
glindblom <lindblom.gustav@gmail.com>
2016-09-14 23:20:01 +0200

    Changes to the test structure, due to a change in the gameboard logic.

---
+++ GameOfLife.Tests/CellTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameOfLife;
+
+namespace GameOfLife.Tests
+{
+    [TestClass]
+    public class CellTests
+    {
+        [TestMethod]
+        public void CellsShouldNotifyWhenStateChanged()
+        {
+            var gameboard = new GameBoard(10, 10);
+            bool eventFired = false;
+            bool eventResult = false;
+
+            gameboard.GetCell(5, 5).PropertyChanged += (e, a) =>
+            {
+                if (!(e is Cell) || a.PropertyName != "Alive") return;
+
+                eventResult = (e as Cell).Alive;
+                eventFired = true;
+            };
+
+            gameboard.GetCell(5, 5).Alive = true;
+
+            Assert.AreEqual(true, eventFired);
+            Assert.AreEqual(true, eventResult);
+        }
+    }
+}
